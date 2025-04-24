611ffef70ed50cdf0e7f3d621bbb0313277ce35d
zoe.mix <zmmix2468@gmail.com>
2025-04-24 14:58:24 -0400

    Removed obsolete files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export { default as MoleculeViewer } from './moleculeViewer/moleculeViewer.jsx';
 export { default as FileParser } from './utils/fileParser.jsx';
-// export { default as UIPanelsOnly } from './moleculeViewer/uiPanels.jsx'
 export { default as UploadButton } from './moleculeViewer/uploadButton.jsx'
 
