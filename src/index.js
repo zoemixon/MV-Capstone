4f19aebe67ef173fb34cc342883ad9ac926525fc
Zaden Connell <zaden.connell@gmail.com>
2025-04-29 14:48:17 -0400

    Add DensityCloudControls component and update index exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as MoleculeViewer } from './moleculeViewer/moleculeViewer.jsx';
 export { default as FileParser } from './utils/fileParser.jsx';
-export { default as UploadButton } from './moleculeViewer/uploadButton.jsx'
+export { default as UploadButton } from './moleculeViewer/uploadButton.jsx';
+export { default as DensityCloudControls } from './utils/densityCloud.jsx';
 
